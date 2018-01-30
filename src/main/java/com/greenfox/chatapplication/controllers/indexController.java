535139413a785a4707c15eac2c4af6d7080ecd91
Fabrizio <fabrizio.finta@gmail.com>
2018-01-30 10:36:59 +0100

    feat(ChatApp): index page added, root controller created

--- src/main/java/com/greenfox/chatapplication/controllers/indexController.java
+++ src/main/java/com/greenfox/chatapplication/controllers/indexController.java
@@ -1,7 +1,14 @@
 package com.greenfox.chatapplication.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class indexController {
+  
+  @GetMapping("")
+  public String getIndexPage(){
+    return "index";
+  }
+  
 }
